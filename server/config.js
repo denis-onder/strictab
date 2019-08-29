b26a63d4bd88e37d6b194d0bd372b0d67080b9b8
denis-onder <denis_onder@protonmail.com>
2019-08-29 03:06:08 +0200

    Database

--- server/config.js
+++ server/config.js
@@ -1,4 +1,10 @@
+require("dotenv").config();
+
 module.exports = {
   port: process.env.PORT || 5000,
-  environment: process.env.NODE_ENV || "development"
+  environment: process.env.NODE_ENV || "development",
+  database: {
+    port: process.env.DB_PORT || 27017,
+    name: process.env.DB_NAME || "strictab"
+  }
 };
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const app = require("express")();
 const path = require("path");
 const config = require("./config");
 const opn = require("opn");
+const mongoose = require("mongoose");
 
 app.listen(config.port, err =>
   err
@@ -9,6 +10,12 @@ app.listen(config.port, err =>
     : console.log(
         `Server running!\nAddress: http://localhost:${config.port}/\nEnvironment: ${config.environment}`
       )
+);
+
+mongoose.connect(
+  `mongodb://localhost:${config.database.port}/${config.database.name}`,
+  { useNewUrlParser: true },
+  err => (err ? console.error(err) : console.log("Database connected!"))
 );
 
 app.get("/", (req, res) =>
