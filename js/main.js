e916517de541c9902bbff6bdefdbac2645c2bae3
denis-onder <denis_onder@protonmail.com>
2019-09-04 00:17:37 +0200

    Basic modal and logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-const columns = [
-  document.getElementById("feed_zero"),
-  document.getElementById("feed_one"),
-  document.getElementById("feed_two")
-];
+const modal = document.getElementById("input_modal");
+const columns = Array.prototype.slice.call(
+  document.getElementsByClassName("grid_column")
+);
+const buttons = Array.prototype.slice.call(
+  document.getElementsByClassName("feed_add_icon")
+);
 
+// Center add buttons if the columns are empty.
 columns.map(column =>
   column.children.length === 1
     ? column.classList.add("center_all")
     : column.classList.remove("center_all")
+);
+
+// Toggle the modal
+buttons.map(button =>
+  button.addEventListener("click", () => {
+    modal.classList.add("modal");
+    document
+      .getElementById("input_modal_exit")
+      .addEventListener("click", () => modal.classList.remove("modal"));
+  })
 );
--- web/main.js
+++ web/main.js
@@ -1,6 +1,18 @@
 "use strict";
 
-var columns = [document.getElementById("feed_zero"), document.getElementById("feed_one"), document.getElementById("feed_two")];
+var modal = document.getElementById("input_modal");
+var columns = Array.prototype.slice.call(document.getElementsByClassName("grid_column"));
+var buttons = Array.prototype.slice.call(document.getElementsByClassName("feed_add_icon")); // Center add buttons if the columns are empty.
+
 columns.map(function (column) {
   return column.children.length === 1 ? column.classList.add("center_all") : column.classList.remove("center_all");
+}); // Toggle the modal
+
+buttons.map(function (button) {
+  return button.addEventListener("click", function () {
+    modal.classList.add("modal");
+    document.getElementById("input_modal_exit").addEventListener("click", function () {
+      return modal.classList.remove("modal");
+    });
+  });
 });
