02a86d29e35dd3d4ad0a2788a13888ac6d2f4134
denis-onder <denis_onder@protonmail.com>
2019-09-03 17:54:26 +0200

    Tweaks

--- scripts/database.sh
+++ scripts/database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'Running Mongo container...'
-docker run --name mongo_db -d mongo
+docker run --name -p 5001:27017 mongo_db -d mongo
 echo 'Stand by...'
 docker exec -it mongo_db mongo bash
