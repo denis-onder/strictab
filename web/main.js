6c5bf68af34a5fc2c1547c277ba7f05c33d1c752
denis-onder <denis_onder@protonmail.com>
2019-09-12 13:49:27 +0200

    Fetching data and icon placement for empty feeds

--- web/main.js
+++ web/main.js
@@ -3,20 +3,35 @@ const modal = $("modal");
 const modalExitButton = $("modal_exit_button");
 const modalFormButton = $("modal_form_button");
 const feeds = [$("feed_one"), $("feed_two"), $("feed_three")];
+const mapIntToWord = ["one", "two", "three"];
 
 // Add feed
-const addFeed = (...feedProps) => {
-  const feed = { ...feedProps };
+const addFeed = (columnName, feedName, feedSubreddit) => {
+  const feed = { columnName, feedName, feedSubreddit };
   let state = [];
   if (localStorage.getItem("feeds") !== null) {
     state = JSON.parse(localStorage.getItem("feeds"));
   }
+  // Check if the state alerady contains a feed for the specific column
+  const filteredState = state.filter(feed => feed.columnName === columnName);
+  if (filteredState.length > 0) {
+    // TODO: Implement some form of a visual error message rather than logging
+    console.log("Column already in use.");
+    return;
+  }
   state.push(feed);
   localStorage.setItem("feeds", JSON.stringify(state));
+  init();
+  modal.style.display = "none";
 };
 
-// Close modal
-modalExitButton.addEventListener("click", () => (modal.style.display = "none"));
+// Read feeds and set cards
+const readFeed = (feed, subreddit) => {
+  fetch(`https://www.reddit.com/r/${subreddit}/top/.json?count=20`)
+    .then(res => res.json())
+    .then(data => console.log(data));
+  console.log(feed.id, subreddit);
+};
 
 // Open modal for the correct feed column
 const openModal = target => {
@@ -30,23 +45,33 @@ const openModal = target => {
     );
 };
 
+// Close modal
+modalExitButton.addEventListener("click", () => (modal.style.display = "none"));
+
 // Create icons
 const createIcon = index => {
-  const mapIntToWord = ["one", "two", "three"];
   const icon = document.createElement("i");
   icon.classList.add("fa", "fa-plus-circle", "feed_column_add");
   icon.setAttribute("data-column", `feed_${mapIntToWord[index]}`);
   icon.addEventListener("click", e => openModal(e.target));
-  return icon;
+  feeds[index].style.justifyContent = "center";
+  feeds[index].appendChild(icon);
 };
 
-// Check the feeds
-(() => {
-  for (let i = 0; i < feeds.length; i++) {
-    if (feeds[i].children.length === 0) {
-      const icon = createIcon(i);
-      feeds[i].appendChild(icon);
-      feeds[i].style.justifyContent = "center";
+// Initialization: Check the feeds
+function init() {
+  const state = JSON.parse(localStorage.getItem("feeds")) || [];
+  // Create the icons
+  feeds.forEach((v, i) => createIcon(i));
+  // Check the state.
+  for (let i = 0; i < state.length; i++) {
+    for (let j = 0; j < feeds.length; j++) {
+      if (feeds[j].id === state[i].columnName) {
+        feeds[j].firstChild.remove();
+        readFeed(feeds[j], state[i].feedSubreddit);
+      }
     }
   }
-})();
+}
+
+init();
